79782d8aebc4d62ee75b126e3aa0fc23ab176177
Olha_Vladova <olha_vladova@epam.com>
2022-07-28 01:37:15 +0300

    add title in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-shop';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('appTitle', {static: false}) appTitle!: ElementRef<HTMLHeadingElement>;
+
+  title = 'This is Angular Shop';
+
+  ngAfterViewInit() {
+    this.appTitle.nativeElement.innerText = this.title;
+  }
 }
